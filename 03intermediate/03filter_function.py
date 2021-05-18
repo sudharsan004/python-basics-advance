44b30436a8d45d7a12bb7f44c83b4d82200eadaa
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-18 16:47:42 -0700

    add some more examples to filter

--- 03intermediate/03filter_function.py
+++ 03intermediate/03filter_function.py
@@ -1,8 +1,8 @@
 vegetables =['ladies finger','potato','beans', 'brinjal']
 
 def containsI(str):
-    if 'i' in str:
-        return str
+    if 'i' in str.lower():
+        return True
 
 def strRev(str):
     return str[::-1]
@@ -11,3 +11,10 @@ print(result)
 # using filter and map togeather
 result = tuple(map(strRev,filter(containsI,vegetables)))
 print(result)
+
+reverse_vegetable_list = list(map(strRev,vegetables))
+print(reverse_vegetable_list)
+filter_reverse_vegetables_having_e = list(filter((lambda str: True if ('e' in str.lower()) else False),reverse_vegetable_list))
+print(filter_reverse_vegetables_having_e)
+undo_reverse = set(map(strRev,filter_reverse_vegetables_having_e))
+print('vegetables having e',undo_reverse)
