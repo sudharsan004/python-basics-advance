fcb90911621cfbba01998406bd768055f0e5afee
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-18 14:57:19 -0700

    static and class methods

--- 02oops/05staticmethods_classmethods.py
+++ 02oops/05staticmethods_classmethods.py
@@ -1,14 +1,37 @@
 class Person(object):
-    names =[]
+    # this class variables are common to all the instance of this(and also sub classes) objects, can also be called directly from this class itself.
+    # class variable
+    person_names =[]
+    persons =[]
     def __init__(self,name,age):
-        self.names.append(self)
+        self.person_names.append(name)
+        self.persons
         self.name= name
         self.age = age
+    
+    # class method
+    @classmethod
+    def get_no_persons(obj):
+        return len(obj.person_names)
+
+    # static method like a normal fun inside a class
+    @staticmethod
+    def greet():
+        return('hello world')
 
 class Teacher(Person):
     pass
 
-t= Teacher('s','20')
-print(t.name)
-print(object)
+t1= Teacher('teacher','20')
+print(t1.person_names)
+print(Person.get_no_persons())
+print(Person.person_names)
+t2= Person('s','20')
+print(t2.person_names)
+print(t1.person_names)
+print(Person.get_no_persons())
+print(t2.get_no_persons())
+print(Person.greet())
+
+# class methods and class variables can be called directly by the class or by an instance
         
