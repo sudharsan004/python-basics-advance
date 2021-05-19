19e8f2ca6c209f4125c3d885ac4d0d2140c4a351
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-19 15:49:31 -0700

    update file contents

--- 02oops/04Overloading.py
+++ 02oops/04Overloading.py
@@ -4,16 +4,22 @@ class Point():
         self.y=y
         self.coordinates =(x,y)
     
+    # Dunder methods
     # the functions between the objs can be defined in the class via add sub mul etc methods ..
     def __add__(self, p):
         return Point(self.x+p.x,self.y+p.y)
 
+    def __mul__(self, p):
+        return Point(self.x*p.x,self.y*p.y)
+
     def __str__(self):
-        return str(self.coordinates)
+        return f'coordinates are: {str(self.coordinates)}'
 
 
 p1=Point(5,3)
 p2=p1+p1
 print(p2)
+p3=p2*p1
+print(p3)
 
     
