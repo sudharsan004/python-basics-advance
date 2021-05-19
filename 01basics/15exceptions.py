565fe9ff69e1c7c4120773b730a3699878001111
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-19 16:21:37 -0700

    update exception handling

--- 01basics/15exceptions.py
+++ 01basics/15exceptions.py
@@ -6,4 +6,26 @@ try:
     phone = int(phone)
     print(phone)
 except ValueError:
-    print(ValueError)    
+    print("Enter only phone numbers!")    
+
+# Python program to handle simple runtime error
+
+a = [1, 2, 3]
+try:
+	print (f"Second element = {(a[1])}")
+
+	# Throws error since there are only 3 elements in array
+	print (f"Fourth element = {(a[3])}")
+
+except IndexError:
+	print ("An error occurred")
+
+finally:
+    print('This always executes!')
+
+# Raising and Exception
+
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
