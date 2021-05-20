88b3564ee079e302263a51e30cbfa2831506f57e
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-20 11:27:17 -0700

    decorators concept

--- 04advance/03decorators.py
+++ 04advance/03decorators.py
@@ -3,12 +3,14 @@ Decorators allow us to wrap another function in order to extend the behavior of 
 without permanently modifying it
 '''
 import time
-def timer(function,*args,**kwargs):
-    startTime = time.time()
-    result=function(*args,**kwargs)
-    endTime = time.time()
-    timeTaken = (endTime-startTime)
-    return timeTaken
+def timer(function):
+    def wrapper(*args,**kwargs):
+        startTime = time.time()
+        result=function(*args,**kwargs)
+        endTime = time.time()
+        timeTaken = (endTime-startTime)
+        return (timeTaken,result)
+    return wrapper
 
 def addNumbers(start,end):
     sum=0
@@ -18,17 +20,29 @@ def addNumbers(start,end):
         time.sleep(0.01)
     return sum
 
-timeTaken = timer(addNumbers,5,100)
-print(timeTaken)
+addNumbers = timer(addNumbers)
+result=addNumbers(1,100)
+print(result)
 
 # This can also be achived by decorators
 @timer
 def addNumbers(start,end):
     sum=0
-    for num in range(start,end):
+    for num in range(start,end+1):
         sum += num
         # dealy some time to see the result
         time.sleep(0.01)
     return sum
-    
-addNumbers(1,100)
+
+@timer
+def multiply(start,end):
+    mul=1
+    for num in range(start,end):
+        mul *= num
+    return mul
+
+result=addNumbers(1,100)
+print(result)
+
+multiply_result = multiply(1,10)
+print(multiply_result)
